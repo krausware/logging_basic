b5830fd3a160b2b4a729c37bf40ef418e6874deb
Jonathan Krausfeldt <61332412+krausware@users.noreply.github.com>
2021-03-24 21:20:30 -0500

    Add files via upload

---
+++ scheduler.ps1
@@ -0,0 +1,12 @@
+﻿$eventaction = New-ScheduledTaskAction 
+    -execute 'powershell.exe'
+    -argument -file C:\backup_logs\Event_scraper.ps1
+$eventtrigger = New-ScheduledTaskTrigger -Daily -at 8PM
+$taskname = 'Scraping the logs boss'
+$description = 'looking for common signs of system compromise'
+
+Register-ScheduledTask `
+    -TaskName $taskName `
+    -Action $eventaction `
+    -Trigger $eventrigger `
+    -Description $description
